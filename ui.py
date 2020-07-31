7ad76d1dd596acd68ecd62ee2a23a57d968f6763
Kazephil <kazephil@gmail.com>
2020-07-31 19:58:42 +0900

    Brouillon pour l'interface

---
+++ ui.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+'''
+Définir les éléments graphiques pour l'interface des outils de TAO
+'''
+
+import tkinter as tk
+from tkinter import ttk
+
+dialogue = tk.Tk()
+dialogue.attributes('-topmost', True)
+
+# dialogue.withdraw()
+
